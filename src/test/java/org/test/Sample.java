a4a5c834f8e78e257a31d582279334c6f2df9bb1
Shudeep98 <shudeeppk@gmail.com>
2024-09-12 21:35:06 +0530

    1st code added

---
+++ src/test/java/org/test/Sample.java
@@ -0,0 +1,17 @@
+package org.test;
+
+public class Sample {
+	
+	public void tc1() {
+		System.out.println("Sample1");
+	}
+	
+	public void tc2() {
+		System.out.println("Sample2");
+	}
+	
+	public void tc3() {
+		System.out.println("Sample3");
+	}
+
+}
